bbe2c48fae254595ce0a6629d0997077da67506b
meghxroy <165138599+meghxroy@users.noreply.github.com>
2025-04-03 04:57:48 +0530

    updated ingredient item

--- lib/components/ingredient_item.dart
+++ lib/components/ingredient_item.dart
@@ -10,9 +10,11 @@ class IngredientItem extends StatelessWidget {
     required this.imageUrl,
   });
 
+ @override
   Widget build(BuildContext context) {
     final h = MediaQuery.of(context).size.height;
     final w = MediaQuery.of(context).size.width;
+    var myBox=Hive.box('shopping');
     return Container(
       margin: EdgeInsets.symmetric(vertical: h * 0.01, horizontal: w * 0.33),
       padding: EdgeInsets.only(
@@ -23,16 +25,57 @@ class IngredientItem extends StatelessWidget {
       ),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(10),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
+            color: Colors.grey.withOpacity(0.2),
             spreadRadius: 2,
-            blurRadius: 7,
+            blurRadius: 5,
             offset: Offset(0, 3), // changes position of shadow
           ),
         ],
       ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(imageurl,width: w*.2,height: h*.1,fit: BoxFit.cover,),
+          ),
+          Text("$food\n$quantity $measure",style: TextStyle(
+            fontSize: w*.04,fontWeight: FontWeight.bold,letterspacing: 1
+          ),),Sizedbox(width: w*.033,),
+          ValueListenableBuilder(
+            valueListenable:myBox.listenable(),
+            builder: (content,box,_){
+              bool exist=box.containsKey(food);
+              if(exist){
+                return GestureDectector(
+                  onTap: (){
+                    myBox.delete(food);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('item deleted'))
+                    )
+                  }
+                  child: Icon(Icons.done,color: Colors.green,size: w*.07));
+              }
+              else { 
+                return GestureDetector(
+                  onTap:(){
+                    String value='$food $measure $quantity';
+                    myBox.put(food, value);
+                    ScoffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('item added successfully'))
+                    )
+                  },
+                child: Icon(
+                Icons.add_circle_outline_rounded,size: w*.07,color: Colors.red,)
+                );
+              }
+            },
+          )
+        ]
+      )
     );
   }
 }
