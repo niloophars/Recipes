1e6789c83aeacbf954d87330fe314616e142eafc
minha <h2gkmo.min@gmail.com>
2025-04-03 13:28:47 +0530

    messagee

---
+++ lib/components/all_recipe.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import '../components/custom_app_bar.dart';
+
+class AllRecipe extends StatelessWidget {
+  final String recipe;
+  const AllRecipe({super.key, required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    final h = MediaQuery.of(context).size.height;
+    final w = MediaQuery.of(context).size.width; // Used for layout calculations
+    return Scaffold(
+      appBar: CustomAppBar(title: recipe, back: true),
+
+      body: FutureBuilder(
+        future: ConstantFunction.getResponse(
+          recipe),
+        // Ensure this function is defined and returns a valid Future
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+           }else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text('no data'));
+          }
+
+          return Padding(
+            padding: EdgeInsets.only(
+              right: w * .034, // Correctly using 'w'
+              left: w * .034,
+              top: h * 0.3,
+            ),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 1,
+                crossAxisSpacing: 10,
+                childAspectRatio: 0.6,
+              ),
+              itemCount:
+                  (snapshot.data as List<dynamic>?)?.length ?? 0, // Cast snapshot.data to List
+              itemBuilder: (context, index) {
+                Map<String, dynamic> snap =
+                    (snapshot.data as List<Map<String, dynamic>>)[index];
+                int time = snap['totalTime']?.toInt() ?? 0;
+                return Container(
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Stack(
+                          children: [
+                            GestureDetector(
+                              onTap: (){
+                                Navigator.pushNamed(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => RecipeDetails(recipe: snap['label'])),
+                              },
+                            )
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(snap['image']),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 7,
+                              left: 15,
+                              child: Container(
+                                height: 20,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                  color: Colors.redAccent,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "${time.toString()} min",
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 6, top: 8),
+                        child: Text(
+                          snap['label'],
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: w * .03,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/components/ingredient_item.dart
+++ lib/components/ingredient_item.dart
@@ -26,10 +26,38 @@ class IngredientItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
+            color: Colors.grey.withOpacity(0.2),
             spreadRadius: 2,
-            blurRadius: 7,
-            offset: Offset(0, 3), // changes position of shadow
+            blurRadius: 5,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              imageUrl,
+              height: h * 0.1,
+              width: w * 0.2,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            "$food\n$quantity $measure",
+            style: TextStyle(
+              fontSize: h * 0.02,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1,
+            ),
+          ),
+          SizedBox(width: w * 0.02),
+          Icon(
+            Icons.add_circle_outline,
+            size: w * 0.07,
+            color: Colors.redAccent,
           ),
         ],
       ),
